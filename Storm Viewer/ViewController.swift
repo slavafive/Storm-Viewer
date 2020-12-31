ee8e3d449022eaa4f41d6f4e180c644be6fa8405
Vyacheslav Efimov <efimov@element-soft.com>
2020-12-31 14:33:35 +0300

    Initial Commit

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Вячеслав Ефимов on 31.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
