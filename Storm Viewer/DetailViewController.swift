a07f8311e0fb604a209af7af2b48685a2abf1b0f
Vyacheslav Efimov <efimov@element-soft.com>
2021-01-02 22:08:36 +0300

    Add UIActivityViewController

--- Storm Viewer/DetailViewController.swift
+++ Storm Viewer/DetailViewController.swift
@@ -20,6 +20,11 @@ class DetailViewController: UIViewController {
         
         title = displayedImageName
         
+        // action parameter tells: "when you're tapped, call the shareTapped() method"
+        // target tells the button that the method belongs to the current controller
+        // #selector tells the Swift compiler that a method called "shareTapped" will exist, and should be triggered when the button is tapped
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -35,6 +40,22 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.navigationBar.tintColor = .purple
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    // @objc means the this method will get called by the underlying Objective-C operating system
+    @objc func shareTapped() {
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        
+        // tell iOS where the activity view controller should be anchored (has effect only on iPad)
+        // applicationActivities is related to the external app's services
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
